9ab0722515754e6793afca90c063a66c0be53364
Everton Maldonado <everton.maldonado@gmail.com>
2015-12-03 20:26:36 -0500

    extract blame files of all files trying to fix the problem with files with the same name.

--- sql_scripts/create_technical_debt_summary.sql
+++ sql_scripts/create_technical_debt_summary.sql
@@ -52,13 +52,10 @@ CREATE TABLE tags_information (
 
 -- 5
 update technical_debt_summary set comment_classification = 'REQUIREMENT' where comment_classification = 'IMPLEMENTATION';
-
 update technical_debt_summary set project_name = 'apache-ant' where project_name = 'apache-ant-1.7.0';
 update technical_debt_summary set version_name = 'ANT_170', version_hash = '6bfe7759b0d7662f764a6efd97436b48aa74da2a' where project_name = 'apache-ant';
-
 update technical_debt_summary set project_name = 'apache-jmeter' where project_name = 'apache-jmeter-2.10';
 update technical_debt_summary set version_name = 'v2_10', version_hash = '05e19dd900305b43296a89a8fbbd4669b987546b' where project_name = 'apache-jmeter';
-
 update technical_debt_summary set project_name = 'jruby' where project_name = 'jruby-1.4.0';
 update technical_debt_summary set version_name = '1.4.0', version_hash = '69fbfa336591fb1a65d4000556b3fedda30baf8f' where project_name = 'jruby';
 
@@ -74,8 +71,27 @@ CREATE TABLE file_directory_per_version (
   matched_analyzed_file_directory text
 );
 
--- 7
-pg_dump -Fc -Uevermal comment_classification > ~/Downloads/technical_debt_summary_after_cleaning.dump
+--7
+alter table file_directory_per_version add column repository_directory text;
+
+--8
+drop table if exists file_blame_per_version;
+CREATE TABLE file_blame_per_version (
+  project_name text, 
+  version_name text,
+  version_hash text,
+  version_order text,
+  file_name text,
+  file_directory text, 
+  commit_short_hash text,
+  author_name text,
+  author_date timestamp with time zone,
+  file_line numeric,
+  line_content text
+);
+
+-- aux --
+pg_dump -Fc -Uevermal comment_classification > ~/Downloads/technical_debt_summary_investigation.dump
 
 -- create script to clean the file directory before copying the csv file
 copy (select * from technical_debt_summary) to '/Users/evermal/Downloads/technical_debt_summary.csv' (format csv,  header true);
@@ -87,13 +103,11 @@ with temp as (
   )
 update technical_debt_summary set file_directory = t.file_directory, version_introduced_name = t.version_introduced_name, version_introduced_hash = t.version_introduced_hash, version_introduced_file_directory = t.version_introduced_file_directory, version_removed_name = t.version_removed_name,version_removed_hash = t.version_removed_hash,version_removed_file_directory = t.version_removed_file_directory,last_version_that_comment_was_found_name = t.last_version_that_comment_was_found_name,last_version_that_comment_was_found_hash = t.last_version_that_comment_was_found_hash,last_version_that_comment_was_found_file_directory = t.last_version_that_comment_was_found_file_directory   from temp t where t.processed_comment_id = technical_debt_summary.processed_comment_id;
 
-
-
-
-
 update technical_debt_summary set version_introduced_name  = null, version_introduced_hash = null, version_removed_name = null, version_removed_hash = null, last_version_that_comment_was_found_name = null, last_version_that_comment_was_found_hash = null;
-
-
 
 select * from file_directory_per_version where version_hash = '92dd8b805b5fc4ae4821ad9713840a99bc0ff2eb' and file_name = 'RubyIO.java';
 
+select * from file_directory_per_version where file_name ='IR_Builder.java';
+
+regex to parse blame files
+-- 
