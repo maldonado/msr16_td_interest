f0ae4fcaf6aaf66da2596c498c0dc3ac77b7777b
Yasutaka Kamei <kamei@ait.kyushu-u.ac.jp>
2016-04-14 14:53:16 -0400

    add data and scripts to generate the data for non-SATD

--- python_scripts/calculate_interests_non-SATD.py
+++ python_scripts/calculate_interests_non-SATD.py
@@ -1,5 +1,6 @@
 import csv
 import re
+import sys
 
 # setting
 home_dir = '/Users/kamei/Research/techdebt/msr16_td_interest'
@@ -12,17 +13,79 @@ v3=0
 class Metrics:
     def __init__(self):
         self.version_name = -1
-        self.introduce = -1
-        self.last_found = -1
-    
+        self.count_input_introduce = -1
+        self.count_input_last_found = -1
+        self.count_output_introduce = -1
+        self.count_output_last_found = -1
+        self.count_line_introduce = -1
+        self.count_line_last_found = -1
+        self.cyclomatic_introduce = -1
+        self.cyclomatic_last_found = -1
+        self.max_nesting_introduce = -1
+        self.max_nesting_last_found = -1
+            
     def out(self,sep="#"):
         return sep.join([str(self.introduce), str(self.last_found)])
 
     def out_all(self,sep="#"):
-        return sep.join([str(self.version_name), str(self.introduce), str(self.last_found)])
+        return sep.join([str(self.version_name), str(self.count_input_introduce), str(self.count_input_last_found), str(self.count_output_introduce), str(self.count_output_last_found),
+                         str(self.count_line_introduce), str(self.count_line_last_found), str(self.cyclomatic_introduce), str(self.cyclomatic_last_found), str(self.max_nesting_introduce), str(self.max_nesting_last_found)])
 
+    #count_input.out_all(), count_output.out(), count_line.out(), cyclomatic.out(), max_nesting.out()
+
+class MetricsManeger():
+    def __init__(self):
+        self.list = {}
+        
+    def add_version(self, key, value):
+        if self.list.has_key(key):
+            print "duplicate@add " + key
+        else:
+            m = Metrics()
+            m.version_name = value
+            self.list[key] = m
+            #print "1:" + key
+            
+    def add_introduce_version(self, key, count_input, count_output, count_line, cyclomatic, max_nesting):
+        if self.list.has_key(key):
+            m = self.list.get(key)
+            if m.count_input_introduce == -1:
+                m.count_input_introduce = count_input
+                m.count_output_introduce = count_output
+                m.count_line_introduce = count_line
+                m.cyclomatic_introduce = cyclomatic
+                m.max_nesting_introduce = max_nesting
+                #print "2:" + key
+            else:
+                print "duplicate@intro " + key
+        else:
+            m = Metrics()
+            m.version_name = count_input
+            m.count_input_introduce = count_input
+            m.count_output_introduce = count_output
+            m.count_line_introduce = count_line
+            m.cyclomatic_introduce = cyclomatic
+            m.max_nesting_introduce = max_nesting
+            self.list[key] = m            
+            
+    def add_last_version(self, key, count_input, count_output, count_line, cyclomatic, max_nesting):
+        if self.list.has_key(key):
+            m = self.list.get(key)
+            if m.count_input_last_found == -1:
+                m.count_input_last_found = count_input
+                m.count_output_last_found = count_output
+                m.count_line_last_found = count_line
+                m.cyclomatic_last_found = cyclomatic
+                m.max_nesting_last_found = max_nesting
+                #print "3:" + key
+
+            else:
+                print "duplicate@last "  + key
+                
 # Run runUND.pl if there is *.und file
-if __name__ == "__main__":
+if __name__ == "__main__":               
+    #sys.exit()
+    
     #debt_file = home_dir + '/datasets/CSV/technical_debt_summary_test.csv'
     debt_file = home_dir + '/datasets/CSV/technical_debt_summary.csv'
     tags_file = home_dir + '/datasets/CSV/tags_information.csv'
@@ -66,6 +129,8 @@ if __name__ == "__main__":
     #metrics_columns = ["Kind", "Name"]
     count = 0
     
+    #analyazed = {}
+    
     with open(debt_file) as csvfile:
         f = csv.reader(csvfile)
         
@@ -73,7 +138,8 @@ if __name__ == "__main__":
         f_CountInput.write("#".join(["Project","Type","File_Name","Class_Name","Method_Signature","v1","v1_date","v2","v2_date","version_name","CountInput_v1","CountInput_v2","CountOutput_v1","CountOutput_v2","CountLine_v1","CountLine_v2","Cyclomatic_v1","Cyclomatic_v2","MaxNesting_v1","MaxNesting_v2\n"]))
         #f_CountInput.write("#".join(["Method_Signature","version_name","CountInput_v1","CountInput_v2\n"]))
         
-        for line in f:          
+        for line in f:
+            list = MetricsManeger()
             count = count + 1
             
             if count > 2000:
@@ -88,14 +154,8 @@ if __name__ == "__main__":
             
             idxs = [_version,_version_introduced_name,_last_version_that_comment_was_found_name]
 
-            print "###".join([line[_project],line[_class_name],line[_file_name], line[_function_signature]])
-            
-            count_input = Metrics()
-            count_output = Metrics()
-            count_line = Metrics()
-            cyclomatic = Metrics()
-            max_nesting = Metrics()
-            
+            #print "###".join([line[_project],line[_class_name],line[_file_name], line[_function_signature]])
+                        
             for idx in idxs:        
                 if line[idx] == '' or line[idx] == 'not_removed':
                     if line[_project] == 'apache-ant':
@@ -109,17 +169,19 @@ if __name__ == "__main__":
                                 
                 metrics_file_base = "/".join([und_out_dir, line[_project], line[idx]])
                 metrics_method_file = metrics_file_base + ".method-level.product.csv"
-                
-                #method_sig = line[_function_signature]
-                # change the format
-                
+
+                #if analyazed.has_key(metrics_method_file+line[_file_name]):
+                #    print "skip:" + metrics_method_file+line[_file_name]
+                #    continue
+                #analyazed[metrics_method_file+line[_file_name]] = 1
+                                                
                 # case313_line1178(java.lang.Object, java.lang.Object[], int) => case313_line1178(Object, Object[], int)
                 method_sig = re.sub( r'[a-zA-Z0-9]*\.','',line[_function_signature]) 
                 method_sig = re.sub( r'<.*?>', '', method_sig)
                 # case313_line1178(Object, Object[], int) => case313_line1178(Object,Object[],int)
                 method_sig = method_sig.replace(' ','')
                 method_sig = ".".join([line[_class_name], method_sig])
-                print "    " + line[idx] +  ":" + method_sig
+                #print "    " + line[idx] +  ":" + method_sig
                 
                 met_idx = 3
                                                 
@@ -127,31 +189,33 @@ if __name__ == "__main__":
                 f2 = csv.reader(tmp_f2)
                 
                 for metrics_line in f2:
-                    metrics_line[1] = metrics_line[1].replace('...','[]')                    
+                    # skip header
+                    if metrics_line[0].find("Method") == -1:
+                        continue
+                    
+                    # skip non same file
+                    if metrics_line[2].find(line[_file_name]) == -1:
+                        continue
+                    
+                    metrics_line[1] = metrics_line[1].replace('...','[]')                                        
+                    # skip function in non main class
+                    if metrics_line[1].find(".(Anon_") != -1:
+                        continue
+                    
                     # same method name
-                    if method_sig == metrics_line[1]: #TODO magic number
-                        print "    =>" + metrics_line[met_idx]
+                    if method_sig != metrics_line[1]: #TODO magic number
+                        key = metrics_line[1] + "@" + metrics_line[2]
+                        #print "    =>" + metrics_line[1] + " " + metrics_line[met_idx]
                         
                         if idx == _version:
-                            count_input.version_name = metrics_line[met_idx]
-                            v1 = v1 + 1
+                            list.add_version(key, metrics_line[met_idx])
+                                                        
                         if idx == _version_introduced_name:
-                            count_input.introduce = metrics_line[met_idx]
-                            count_output.introduce = metrics_line[8]
-                            count_line.introduce = metrics_line[4]
-                            cyclomatic.introduce = metrics_line[13]
-                            max_nesting.introduce = metrics_line[17]
+                            list.add_introduce_version(key, metrics_line[met_idx], metrics_line[8], metrics_line[4], metrics_line[13], metrics_line[17])
                             
-                            v2 = v2 + 1
                         if idx == _last_version_that_comment_was_found_name:
-                            count_input.last_found = metrics_line[met_idx]
-                            count_output.last_found = metrics_line[8]
-                            count_line.last_found = metrics_line[4]
-                            cyclomatic.last_found = metrics_line[13]
-                            max_nesting.last_found = metrics_line[17]
-                            
-                            v3 = v3 + 1
-            
+                            list.add_last_version(key, metrics_line[met_idx], metrics_line[8], metrics_line[4], metrics_line[13], metrics_line[17])
+    
             date_introduced = ""
             date_last_found = ""
             try:
@@ -163,7 +227,8 @@ if __name__ == "__main__":
                 date_last_found = revs[line[_last_version_that_comment_was_found_name]]                                                   
             except KeyError:
                 date_introduced = tags[line[_last_version_that_comment_was_found_name]]  
-
-            f_CountInput.write("#".join([line[_project],line[_comment_classification],line[_file_name],line[_class_name],line[_function_signature],line[_version_introduced_name],date_introduced,line[_last_version_that_comment_was_found_name],date_last_found,count_input.out_all(), count_output.out(), count_line.out(), cyclomatic.out(), max_nesting.out() + '\n']))
-        f_CountInput.close()        
-print "/".join([str(v1),str(v2),str(v3)])                          
+            
+            for key in list.list.iterkeys():
+                m = list.list[key]
+                f_CountInput.write("#".join([line[_project],line[_comment_classification],line[_file_name],line[_class_name],key,line[_version_introduced_name],date_introduced,line[_last_version_that_comment_was_found_name],date_last_found, m.out_all() + '\n']))
+        f_CountInput.close()
