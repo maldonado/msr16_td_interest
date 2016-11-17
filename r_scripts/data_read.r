591b5f45b3445f0c68c961023b69dc3c9e3104c8
Yasutaka Kamei <kamei@ait.kyushu-u.ac.jp>
2016-11-17 17:06:17 -0500

    update the way to calculate the interest based on reviewers' comment

--- r_scripts/data_read.r
+++ r_scripts/data_read.r
@@ -9,11 +9,11 @@ data.s2 <- data.s1[(data.s1[, "CountInput_v1"] != -1 & data.s1[, "CountInput_v2"
 
 # only use technical debt including non 0 for division
 data.CountInput <- data.s2[(data.s2[,"CountInput_v1"] != 0), ]
-data.CountInput.all <- cbind(data.CountInput, interest = ((data.CountInput$CountInput_v2 - data.CountInput$CountInput_v1) / data.CountInput$CountInput_v2 * 100))
+data.CountInput.all <- cbind(data.CountInput, interest = ((data.CountInput$CountInput_v2 - data.CountInput$CountInput_v1) / data.CountInput$CountInput_v1 * 100))
 data.CountInput.positive <- subset(data.CountInput.all, data.CountInput.all$CountInput_v2 > data.CountInput.all$CountInput_v1)
 data.CountInput.negative <- subset(data.CountInput.all, data.CountInput.all$CountInput_v2 < data.CountInput.all$CountInput_v1)
 
 data.CountLine <- data.s2[(data.s2[,"CountLine_v1"] != 0), ]
-data.CountLine.all <- cbind(data.CountLine, interest = ((data.CountLine$CountLine_v2 - data.CountLine$CountLine_v1) / data.CountLine$CountLine_v2 * 100))
+data.CountLine.all <- cbind(data.CountLine, interest = ((data.CountLine$CountLine_v2 - data.CountLine$CountLine_v1) / data.CountLine$CountLine_v1 * 100))
 data.CountLine.positive <- subset(data.CountLine.all, data.CountLine.all$CountLine_v2 > data.CountLine.all$CountLine_v1)
 data.CountLine.negative <- subset(data.CountLine.all, data.CountLine.all$CountLine_v2 < data.CountLine.all$CountLine_v1)
