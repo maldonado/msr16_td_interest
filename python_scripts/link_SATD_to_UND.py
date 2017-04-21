92ab1a46f7db237bf29fb49fbc7176d48cb371ca
Yasutaka Kamei <kamei@ait.kyushu-u.ac.jp>
2017-04-21 14:34:44 -0400

    finish non satd analysis

--- python_scripts/link_SATD_to_UND.py
+++ python_scripts/link_SATD_to_UND.py
@@ -14,6 +14,92 @@ class Metrics:
     def out_all(self,sep="#"):
         return sep.join([str(self.version_name), str(self.introduce), str(self.last_found)])
 
+class NonSATD:
+    def __init__(self):
+        self.project_name = -1
+        self.long_file_name = ""
+        self.count_input_introduce = -1
+        self.count_input_last_found = -1
+        self.count_output_introduce = -1
+        self.count_output_last_found = -1
+        self.count_line_introduce = -1
+        self.count_line_last_found = -1
+        self.cyclomatic_introduce = -1
+        self.cyclomatic_last_found = -1
+        self.max_nesting_introduce = -1
+        self.max_nesting_last_found = -1
+            
+    def out(self,sep="#"):
+        return sep.join([str(self.introduce), str(self.last_found)])
+
+    def out_all(self,sep="#"):
+        return sep.join([str(self.project_name), str(self.long_file_name), str(self.count_input_introduce), str(self.count_input_last_found), str(self.count_output_introduce), str(self.count_output_last_found),
+                         str(self.count_line_introduce), str(self.count_line_last_found), str(self.cyclomatic_introduce), str(self.cyclomatic_last_found), str(self.max_nesting_introduce), str(self.max_nesting_last_found)])
+
+# non-SATD
+class NonSATDManeger():
+    def __init__(self):
+        self.list = {}
+           
+    def add_introduce_version(self, key, project_name, long_file_name, count_input, count_output, count_line, cyclomatic, max_nesting):
+        if self.list.has_key(key):
+            m = self.list.get(key)
+            if m.count_input_introduce == -1:
+                m.count_input_introduce = count_input
+                m.count_output_introduce = count_output
+                m.count_line_introduce = count_line
+                m.cyclomatic_introduce = cyclomatic
+                m.max_nesting_introduce = max_nesting
+                #print "2:" + key
+            else:
+                if self.list[key].long_file_name == long_file_name:
+                    if long_file_name == "src/main/org/apache/tools/ant/ComponentHelper.java":
+                        # src/main/org/apache/tools/ant/ComponentHelper.java is exception
+                        # that one is added in first verison
+                        m.count_input_introduce = count_input
+                        m.count_output_introduce = count_output
+                        m.count_line_introduce = count_line
+                        m.cyclomatic_introduce = cyclomatic
+                        m.max_nesting_introduce = max_nesting                        
+                    else:
+                        print "duplicate@intro "  + key
+                        print "   " + self.list[key].long_file_name + " v.s. " + long_file_name
+                        print "   " + self.list[key].count_input_introduce + " v.s. " + count_input
+                else:
+                    if self.list[key].long_file_name != "src/main/org/apache/tools/ant/ComponentHelper.java":
+                        print "not duplicate@intro "  + key
+                        print "   " + self.list[key].long_file_name + " v.s. " + long_file_name
+        else:
+            m = NonSATD()
+            m.project_name = project_name
+            m.long_file_name = long_file_name
+            m.count_input_introduce = count_input
+            m.count_output_introduce = count_output
+            m.count_line_introduce = count_line
+            m.cyclomatic_introduce = cyclomatic
+            m.max_nesting_introduce = max_nesting
+            self.list[key] = m            
+            
+    def add_last_version(self, key, long_file_name, count_input, count_output, count_line, cyclomatic, max_nesting):
+        if self.list.has_key(key):
+            m = self.list.get(key)
+            if m.count_input_last_found == -1:
+                m.count_input_last_found = count_input
+                m.count_output_last_found = count_output
+                m.count_line_last_found = count_line
+                m.cyclomatic_last_found = cyclomatic
+                m.max_nesting_last_found = max_nesting
+                #print "3:" + key
+
+            else:
+                if self.list[key].long_file_name == long_file_name:
+                    print "duplicate@last "  + key
+                    print "   " + self.list[key].long_file_name + " v.s. " + long_file_name
+                else:
+                    if self.list[key].long_file_name != "src/main/org/apache/tools/ant/types/resources/StringResource.java":
+                        print "not duplicate@last "  + key
+                        print "   " + self.list[key].long_file_name + " v.s. " + long_file_name
+
 def link_SATD_to_UND(project, versions, file_names, function_signatures):
     print "--"
     print project
@@ -81,7 +167,7 @@ def link_SATD_to_UND(project, versions, file_names, function_signatures):
                     max_nesting.last_found = line[u'MaxNesting']
         
         tmp_f2.close()
-        i = i + 1
+        i = i + 1        
     return [count_input, count_output, count_line, cyclomatic, max_nesting]
 
 def link_NON_SATD_to_UND(project, versions, file_names, function_signatures):
@@ -91,14 +177,9 @@ def link_NON_SATD_to_UND(project, versions, file_names, function_signatures):
     print file_names
     print function_signatures
     print "--"
-    
-    count_input = Metrics()
-    count_output = Metrics()
-    count_line = Metrics()
-    cyclomatic = Metrics()
-    max_nesting = Metrics()
-        
+                
     i=0
+    non_satd = NonSATDManeger()
     for version in versions:
         metrics_file_base = "/".join([s.und_out_dir, project, version])
         metrics_method_file = metrics_file_base + ".method-level.product.csv"
@@ -133,27 +214,22 @@ def link_NON_SATD_to_UND(project, versions, file_names, function_signatures):
             if match:
                 temp_method_sig = match.group(1)
                                         
-            # same method name
-            if method_sig == temp_method_sig:
-                print i
+            # not same method name
+            if method_sig != temp_method_sig:
+                key_filename = file_name # metrics_line[2] is directory name + file name. Even if file name is same, but directory name is changed.
+                key_filename = key_filename.split("/")
+                key_filename = key_filename[(len(key_filename)-1)] #just use file name
+                key = temp_method_sig + "@" + key_filename
+                
                 if i == 0: # for introduced
-                    count_input.introduce = line[u'CountInput']
-                    count_output.introduce = line[u'CountOutput']
-                    count_line.introduce = line[u'CountLine']
-                    cyclomatic.introduce = line[u'Cyclomatic']
-                    max_nesting.introduce = line[u'MaxNesting']
-                    
+                    non_satd.add_introduce_version(key, project, file_name, line[u'CountInput'], line[u'CountOutput'], line[u'CountLine'], line[u'Cyclomatic'], line[u'MaxNesting'])
                 if i == 1: # for last found
-                    count_input.last_found = line[u'CountInput']
-                    count_output.last_found = line[u'CountOutput']
-                    count_line.last_found = line[u'CountLine']
-                    cyclomatic.last_found = line[u'Cyclomatic']
-                    max_nesting.last_found = line[u'MaxNesting']
-        
+                    non_satd.add_last_version(key, file_name, line[u'CountInput'], line[u'CountOutput'], line[u'CountLine'], line[u'Cyclomatic'], line[u'MaxNesting'])        
         tmp_f2.close()
         i = i + 1
-    return [count_input, count_output, count_line, cyclomatic, max_nesting]
 
+    # TODO: will return list
+    return non_satd
 
 if __name__ == "__main__":
     #project = "ant"
--- python_scripts/run_non_SATD.py
+++ python_scripts/run_non_SATD.py
@@ -1,8 +1,4 @@
 import setting_non_SATD as s
-import checkoutRevisions
-import runUND
-import extract_metrics_at_method_level
-import git_comments
 import link_SATD_to_UND
 
 import csv
@@ -65,7 +61,6 @@ def get_func_signature(func_name, func_parameter_list):
      
     return signature
 
-count = 0   
 with open(s.debt_file) as csvfile:
     csvfile = open(s.debt_file)
     reader = csv.DictReader(csvfile)
@@ -77,72 +72,17 @@ with open(s.debt_file) as csvfile:
 
     #for calculate_interest
     fo_interest = open (s.interest_file, 'w')
-    fo_interest.write("#".join(["Project","Type","File_Name","Method_Signature","v1","v1_date","v2","v2_date","version_name","CountInput_v1","CountInput_v2","CountOutput_v1","CountOutput_v2","CountLine_v1","CountLine_v2","Cyclomatic_v1","Cyclomatic_v2","MaxNesting_v1","MaxNesting_v2", "Debt", "Intro_ID", "Intro_Comment","Remove_ID","Remove_Comment\n"]))
-
-    print("#################################################################")
-    print("#for pre-process")
-    print("#################################################################")
-    for line in reader:
-        count = count + 1
-        
-        if count < s.START_LOOP:
-            continue
-        
-        if count > s.MAX_LOOP:
-            break
-        
-        if line[u'comment_location'] != "FUNCTION":
-            continue
-        
-        print line
-        project =  line[u'project']
-        versions = [] #for git_comments
-                
-        for idx in idxs:
-            version = line[idx]
-            if idx != "last_found_commit_hash":  #for git_comments
-                versions.append(version)
-            
-            if idx == "removed_version_commit_hash" and line[u'has_removed_version'] == "f":
-                version = get_latest_version(project)
-
-            checkoutRevisions.checkoutRevision(project, version, count, reuse=True)
-            runUND.runUND(project, version, count, reuse=True)
-            extract_metrics_at_method_level.extract_metrics(project, version, count, reuse=True)
-        
-        #for git_comments
-        debt = git_comments.git_comments(project, versions, count)
-        debt.type = line[u'td_classification']
-        debt.debt = line[u'comment_text']
-        csvWriter_comment_file.writerow(debt.out())
-    fo_comment_file.close()        
-
-    print("#################################################################")
-    print("#for read comments")
-    print("#################################################################")
-    comments_intro = {}
-    comments_remove = {}
-    with open(s.comment_file) as csv_comment_file:
-        csv_comment_file = open(s.comment_file)
-        comment_reader = csv.DictReader(csv_comment_file)
-    
-        for line in comment_reader:
-            # 0:Type 1:Debt 2:Introduce Date 3:Introduce Author 4:Introduce Comment
-            # 5:Remove Date 6:Remove Author 7:Remove Comment 8:Introduce ID 9:Remove ID
-            comments_intro[line[u'Introduce ID']] = line[u'Introduce Comment']
-            comments_remove[line[u'Remove ID']] = line[u'Remove Comment']
+    fo_interest.write("#".join(["Project_name","Long_file_name","CountInput_v1","CountInput_v2","CountOutput_v1","CountOutput_v2","CountLine_v1","CountLine_v2","Cyclomatic_v1","Cyclomatic_v2","MaxNesting_v1","MaxNesting_v2\n"]))
 
     print("#################################################################")
     print("#for calculate_interests")
     print("#################################################################")
     print ""
-    csvfile.seek(0, 0)
-    next(reader)
         
-    count = 0    
+    count = 1 # consider header   
     for line in reader:
         count = count + 1
-        
+                
         if count < s.START_LOOP:
             continue
         
@@ -152,7 +92,7 @@ with open(s.debt_file) as csvfile:
         if line[u'comment_location'] != "FUNCTION":
             continue
         
-        print "P1: ",
+        print str(count) + " [P1]: ",
         print line
         project =  line[u'project']
         td_classification= line[u'td_classification']
@@ -175,34 +115,26 @@ with open(s.debt_file) as csvfile:
             if idx == "removed_version_commit_hash" and line[u'has_removed_version'] == "f":
                 version = get_latest_version(project)
 
-        set_metrics = link_SATD_to_UND.link_SATD_to_UND(project, versions, file_names, function_signatures)
-        count_input, count_output, count_line, cyclomatic, max_nesting = set_metrics
+        non_satd = link_SATD_to_UND.link_NON_SATD_to_UND(project, versions, file_names, function_signatures)
+        for key in non_satd.list.iterkeys():
+            m = non_satd.list[key]
+            fo_interest.write(m.out_all() + '\n')
         
-        debt = line[u'comment_text']
-        debt = debt.replace("#", "")
-        debt = debt.replace('\n','')
-        debt = debt.replace('\r','')
-
-        date_introduced = line[u'introduced_version_date']
-        date_last_found = line[u'last_found_date']
-
-        intro_id = line[u'introduced_version_commit_hash']
-        intro_comment = ""
-        remove_id = line[u'removed_version_commit_hash']
-        remove_comment = ""
-        last_id = line[u'last_found_commit_hash']
-
-        try:
-            intro_comment = comments_intro[intro_id]
-            intro_comment = intro_comment.replace("#", "")
-        except KeyError:
-            intro_comment = ""
+        #count_input, count_output, count_line, cyclomatic, max_nesting = set_metrics
         
-        try:
-            remove_comment = comments_remove[remove_id]
-            remove_comment = remove_comment.replace("#", "")
-        except KeyError:
-            remove_comment = ""
+        #debt = line[u'comment_text']
+        #debt = debt.replace("#", "")
+        #debt = debt.replace('\n','')
+        #debt = debt.replace('\r','')
+
+        #date_introduced = line[u'introduced_version_date']
+        #date_last_found = line[u'last_found_date']
+
+        #intro_id = line[u'introduced_version_commit_hash']
+        #intro_comment = ""
+        #remove_id = line[u'removed_version_commit_hash']
+        #remove_comment = ""
+        #last_id = line[u'last_found_commit_hash']
         
-        fo_interest.write("#".join([project,td_classification,file_names[0], function_signatures[0],intro_id,date_introduced,last_id,date_last_found,count_input.out_all(), count_output.out(), count_line.out(), cyclomatic.out(), max_nesting.out(), debt, intro_id, intro_comment, remove_id, remove_comment  + '\n']))
+        #fo_interest.write("#".join([project,td_classification,file_names[0], function_signatures[0],intro_id,date_introduced,last_id,date_last_found,count_input.out_all(), count_output.out(), count_line.out(), cyclomatic.out(), max_nesting.out(), debt, intro_id, intro_comment, remove_id, remove_comment  + '\n']))
     fo_interest.close()
